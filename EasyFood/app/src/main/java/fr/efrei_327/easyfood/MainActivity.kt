a4a8583d3315c4c810668169d2b7dbb43f2666f9
bilalgu <73354931+bilalgu@users.noreply.github.com>
2021-11-03 13:25:08 +0100

    Add files via upload

---
+++ EasyFood/app/src/main/java/fr/efrei_327/easyfood/MainActivity.kt
@@ -0,0 +1,11 @@
+package fr.efrei_327.easyfood
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
